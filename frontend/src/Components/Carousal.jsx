af1b13c4748602de09a6f913bdf4fadc574aae0e
Juman Saikia <jumansaikia10@gmail.com>
2024-10-06 23:18:29 +0530

    update

--- frontend/src/Components/Carousal.jsx
+++ frontend/src/Components/Carousal.jsx
@@ -8,7 +8,7 @@ const Carousal = () => {
             <img
                src="https://images.pexels.com/photos/356040/pexels-photo-356040.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                alt="carousel-img-1"
-               style={{ width: "100%", height: "500px", objectFit: "cover" }}
+               style={{ width: "100%", height: "550px", objectFit: "cover" }}
             />
          </div>
          <div>
@@ -22,14 +22,14 @@ const Carousal = () => {
             <img
                src="https://images.pexels.com/photos/356040/pexels-photo-356040.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                alt="carousel-img-3"
-               style={{ width: "100%", height: "500px", objectFit: "cover" }}
+               style={{ width: "100%", height: "550px", objectFit: "cover" }}
             />
          </div>
          <div>
             <img
                src="https://images.pexels.com/photos/356040/pexels-photo-356040.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                alt="carousel-img-4"
-               style={{ width: "100%", height: "500px", objectFit: "cover" }}
+               style={{ width: "100%", height: "550px", objectFit: "cover" }}
             />
          </div>
       </Carousel>
